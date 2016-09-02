630ebbb92cf3121a67f478b36e6718adab2cffa0
fin10 <fender64@gmail.com>
2016-09-02 23:22:41 +0900

    changes project folder structure

--- GearBrowser/src/utils.c
+++ GearBrowser/src/utils.c
@@ -4,7 +4,7 @@
 
 const char *
 app_get_resource_n(const char *file_name) {
-	char output[PATH_MAX] = {0, };
+	char *output = malloc(sizeof(char) * PATH_MAX);
 	char *res_path = app_get_resource_path();
 	if (res_path) {
 		snprintf(output, PATH_MAX, "%s%s", res_path, file_name);
