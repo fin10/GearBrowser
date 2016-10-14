3683100cf38d758f188cb0007899de629af3d347
fin10 <fender64@gmail.com>
2016-10-14 23:22:39 +0900

    implements empty page

--- GearBrowser/src/settings.c
+++ GearBrowser/src/settings.c
@@ -1,5 +1,6 @@
 #include <app_preference.h>
 #include "settings.h"
+#include "utils.h"
 
 void
 settings_init(void) {
@@ -11,7 +12,14 @@ settings_init(void) {
 	preference_is_existing(PREF_KEY_LAST_SEARCH_KEYWORD, &existing);
 	if (!existing) preference_set_string(PREF_KEY_LAST_SEARCH_KEYWORD, "");
 	preference_is_existing(PREF_KEY_LAST_URL, &existing);
-	if (!existing) preference_set_string(PREF_KEY_LAST_URL, "http://www.google.com");
+	if (!existing) {
+		const char *scheme = "file://";
+		const char *filePath = app_get_resource_n("www/index.html");
+		unsigned int size = strlen(scheme) + strlen(filePath) + 1;
+		char *dst = malloc(sizeof(char) * size);
+		snprintf(dst, size, "%s%s", scheme, filePath);
+		preference_set_string(PREF_KEY_LAST_URL, dst);
+	}
 }
 
 int
